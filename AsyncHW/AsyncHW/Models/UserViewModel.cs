c6fe2ea7abe0affc33d2f09161b7fc6d77a74411
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-10-30 21:26:23 +0300

    Create UserViewModel class

---
+++ AsyncHW/AsyncHW/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncHW.Models
+{
+    public sealed class UserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+    }
+}
