bcdc32b6f0077c5e2b8fc179af6164bcc6ac95bd
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-11-02 14:21:15 +0300

    Change UserViewModel, AddressViewModel classes and Users, Address json files

--- AsyncHW/AsyncHW/Models/AddressViewModel.cs
+++ AsyncHW/AsyncHW/Models/AddressViewModel.cs
@@ -2,6 +2,7 @@
 {
     public sealed class AddressViewModel
     {
+        public int Id { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public string House { get; set; }
--- AsyncHW/AsyncHW/Models/UserViewModel.cs
+++ AsyncHW/AsyncHW/Models/UserViewModel.cs
@@ -6,6 +6,6 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
-        public AddressViewModel Address { get; set; }
+        public int AddressId { get; set; }
     }
 }
