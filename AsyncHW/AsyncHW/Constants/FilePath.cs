9c4bbe78825ddde59e028c250299c59da2a94770
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-11-03 22:23:05 +0300

    Add FilePath class with constants

---
+++ AsyncHW/AsyncHW/Constants/FilePath.cs
@@ -0,0 +1,8 @@
+﻿namespace AsyncHW.Constants
+{
+    public static class FilePath
+    {
+        public const string UserFilePath = "JSONs/Users.json";
+        public const string AddressFilePath = "JSONs/Addresses.json";
+    }
+}
