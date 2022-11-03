181d1e12469c8d62c5e3830d01f86128b4a66c52
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-11-03 23:29:50 +0300

    Add GetUsersWithAddressesAsync method and some changes in UserService class

--- AsyncHW/AsyncHW/Services/UserService.cs
+++ AsyncHW/AsyncHW/Services/UserService.cs
@@ -6,23 +6,64 @@ namespace AsyncHW.Services
 {
     public sealed class UserService
     {
-        //private List<UserViewModel> _users = new List<UserViewModel>();
-        //private List<AddressViewModel> _addresses = new List<AddressViewModel>();
+        private List<UserViewModel> _users;
+        private List<AddressViewModel> _addresses;
 
         public async Task<List<UserViewModel>> GetUsersAsync()
         {
-            using (var reader = File.OpenRead(FilePath.UserFilePath))
+            if (_users == null)
             {
-                return await JsonSerializer.DeserializeAsync<List<UserViewModel>>(reader);
+                _users = await DeserializeAsync<List<UserViewModel>>(FilePath.UserFilePath);
             }
+
+            return _users;
         }
 
         public async Task<List<AddressViewModel>> GetAddressesAsync()
         {
-            using (var reader = File.OpenRead(FilePath.AddressFilePath))
+            if (_addresses == null)
             {
-                return await JsonSerializer.DeserializeAsync<List<AddressViewModel>>(reader);
+                _addresses = await DeserializeAsync<List<AddressViewModel>>(FilePath.AddressFilePath);
             }
+
+            return _addresses;
+        }
+
+        public async Task<List<UserWithAddressDTOViewModel>> GetUsersWithAddressesAsync()
+        {
+            if (_users == null)
+            {
+                _users = await DeserializeAsync<List<UserViewModel>>(FilePath.UserFilePath);
+            }
+            
+            if (_addresses == null)
+            {
+                _addresses = await DeserializeAsync<List<AddressViewModel>>(FilePath.AddressFilePath);
+            }
+
+            var usersWithAddresses = _users.Join(_addresses,
+                                                 user => user.AddressId,
+                                                 address => address.AddressId,
+                                                 (user, address) => new UserWithAddressDTOViewModel
+                                                 {
+                                                     UserId = user.UserId,
+                                                     FirstName = user.FirstName,
+                                                     LastName = user.LastName,
+                                                     Email = user.Email,
+                                                     BirthDate = user.BirthDate,
+                                                     AddressId = user.AddressId,
+                                                     City = address.City,
+                                                     Street = address.Street,
+                                                     HouseNumber = address.HouseNumber
+                                                 }).ToList();
+
+            return usersWithAddresses; 
+        }
+
+        private async Task<T> DeserializeAsync<T>(string path)
+        {
+            using var reader = File.OpenRead(path);
+            return await JsonSerializer.DeserializeAsync<T>(reader);
         }
     }
 }
