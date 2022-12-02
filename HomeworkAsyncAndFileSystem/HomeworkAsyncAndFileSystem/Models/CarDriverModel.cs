6987434ae4a970f9af1dc86aa191356067190fff
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-12-02 13:46:23 +0300

    not all

---
+++ HomeworkAsyncAndFileSystem/HomeworkAsyncAndFileSystem/Models/CarDriverModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeworkAsyncAndFileSystem.Models
+{
+    public class CarDriverModel
+    {
+        public int CarNumber { get; set; }
+        public string CarName { get; set; }
+        public string CarModel { get; set; }
+        public string DriverFirstName { get; set; }
+        public string DriverLastName { get; set; }
+    }
+}
